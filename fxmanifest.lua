1c8509ccf695245e53141850ad92a4f91a9c41ae
DJblaze <161307474+DJblaze-BlazeCorp@users.noreply.github.com>
2025-12-16 15:35:15 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,9 @@
+-- fxmanifest.lua
 fx_version 'cerulean'
 game 'gta5'
 author 'Generated for Jesse'
 description 'QBCore Karma / Reputation System (v1.8.0) - Modular Config System, Event Gating Only, Language Exports, Debug Lang Support, SQL Handler, Command Config, Admin Tools'
 version '1.8.0'
-
 shared_scripts {
     'config/config.lua',
     'config/commands.lua',
@@ -13,14 +13,11 @@ shared_scripts {
     'config/lang/*.lua',
     'config/lang/debug/*.lua'
 }
-
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
     'server.lua'
 }
-
 client_scripts {
     'client.lua'
 }
-
 lua54 'yes'
