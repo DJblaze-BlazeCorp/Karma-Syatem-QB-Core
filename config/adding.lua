093080bece502e374c507bfecf59648c347e3ae5
DJblaze <161307474+DJblaze-BlazeCorp@users.noreply.github.com>
2025-12-16 14:00:59 -0700

    Update adding.lua

--- config/adding.lua
+++ config/adding.lua
@@ -1,9 +1,8 @@
--- config/adding.lua
 -- Positive karma events (adding karma)
 Config.AddKarmaEvents = {
-    help_civilian      = { amount = 10, reason = 'Helped a civilian' },
-    revived_player     = { amount = 15, reason = 'Revived a downed player' },
-    completed_job      = { amount = 5,  reason = 'Completed legal job' },
-    rescued_injured    = { amount = 12, reason = 'Rescued an injured civilian' },
-    protected_store    = { amount = 20, reason = 'Helped stop a robbery' }
+    help_civilian = { amount = 10, reason = 'Helped a civilian' },
+    revived_player = { amount = 15, reason = 'Revived a downed player' },
+    completed_job = { amount = 5, reason = 'Completed legal job' },
+    rescued_injured = { amount = 12, reason = 'Rescued an injured civilian' },
+    protected_store = { amount = 20, reason = 'Helped stop a robbery' }
 }
