cf32b46a7c6911e03026802e28eeb395fe4a0f19
DJblaze <161307474+DJblaze-BlazeCorp@users.noreply.github.com>
2025-12-18 15:34:20 -0700

    Change reason field to reason_key in karma events

--- config/adding.lua
+++ config/adding.lua
@@ -1,9 +1,8 @@
--- config/adding.lua
 -- Positive karma events (adding karma)
 Config.AddKarmaEvents = {
-    help_civilian = { amount = 10, reason = 'Helped a civilian' },
-    revived_player = { amount = 15, reason = 'Revived a downed player' },
-    completed_job = { amount = 5, reason = 'Completed legal job' },
-    rescued_injured = { amount = 12, reason = 'Rescued an injured civilian' },
-    protected_store = { amount = 20, reason = 'Helped stop a robbery' }
+    help_civilian = { amount = 10, reason_key = 'help_civilian' },
+    revived_player = { amount = 15, reason_key = 'revived_player' },
+    completed_job = { amount = 5, reason_key = 'completed_job' },
+    rescued_injured = { amount = 12, reason_key = 'rescued_injured' },
+    protected_store = { amount = 20, reason_key = 'protected_store' }
 }
