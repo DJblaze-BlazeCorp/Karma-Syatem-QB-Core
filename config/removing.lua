025d9fd88b3bc1fc791fa3a774b2ee7fe9e31074
DJblaze <161307474+DJblaze-BlazeCorp@users.noreply.github.com>
2025-12-18 15:34:08 -0700

    Refactor karma events to use reason_key

--- config/removing.lua
+++ config/removing.lua
@@ -1,9 +1,10 @@
--- config/removing.lua
 -- Negative karma events (removing or subtracting karma)
 Config.RemoveKarmaEvents = {
-    bank_robbery = { amount = -50, reason = 'Bank robbery' },
-    store_robbery = { amount = -25, reason = 'Store robbery' },
-    player_killed = { amount = -5, reason = 'Killed a player' },
-    police_attack = { amount = -15, reason = 'Attacked law enforcement' },
-    hostage_taken = { amount = -25, reason = 'Took a hostage' }
+    store_rob_register = { amount = -20, reason_key = 'store_rob_register' },
+    store_rob_safe = { amount = -30, reason_key = 'store_rob_safe' },
+    bank_robbery = { amount = -50, reason_key = 'bank_robbery' },
+    store_robbery = { amount = -25, reason_key = 'store_robbery' },
+    player_killed = { amount = -5, reason_key = 'player_killed' },
+    police_attack = { amount = -15, reason_key = 'police_attack' },
+    hostage_taken = { amount = -25, reason_key = 'hostage_taken' }
 }
